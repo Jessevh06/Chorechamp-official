3bf9abd486da0fc2f2e6220e25ac348d355890ea
Jesse van Heesch <Jessevanheesch2006@gmail.com>
2025-11-19 14:46:26 +0100

    Make homepage E2E test locator specific

--- Frond-End/chorechamp/tests/e2e/home.spec.ts
+++ Frond-End/chorechamp/tests/e2e/home.spec.ts
@@ -2,5 +2,12 @@ import { test, expect } from '@playwright/test';
 
 test('homepage werkt', async ({ page }) => {
     await page.goto('/');
-    await expect(page.getByText(/chorechamp/i)).toBeVisible();
+
+    // controleer de titel
+    const titel = page.getByRole('heading', { name: /Welkom bij ChoreChamp/i });
+    await expect(titel).toBeVisible();
+
+    // eventueel extra checks:
+    // const link = page.getByRole('link', { name: 'ChoreChamp' });
+    // await expect(link).toBeVisible();
 });
